50ce2bf27f52d4707011222a75b982acd594731d
James Capra <jcapra76@gmail.com>
2020-02-19 10:19:12 -0800

    Changed the small, centered carousel into a full page masterpiece!

--- css/some.js
+++ css/some.js
@@ -4,3 +4,57 @@ soManyFlavors.forEach(flavor => {
     console.log(e.target.value, e.target.checked);
   });
 });
+
+
+<!-- <script>
+const siteTypeButtons = document.querySelectorAll(
+  "label[data-site-type]"
+);
+siteTypeButtons[0].style.backgroundColor = "limegreen";
+
+// function handleSiteTypeButtonClick(event) {
+//   siteTypeButtons.forEach(button => {
+//     button.style.backgroundColor = "";
+//   });
+//   event.currentTarget.style.backgroundColor = "limegreen";
+// }
+
+// siteTypeButtons.forEach(function(typeButton) {
+//   typeButton.addEventListener("click", handleSiteTypeButtonClick);
+// });
+
+siteTypeButtons.forEach(function(typeButton) {
+  typeButton.addEventListener("click", function(event) {
+    siteTypeButtons.forEach(button => {
+      button.style.backgroundColor = "";
+    });
+    event.currentTarget.style.backgroundColor = "limegreen";
+  });
+});
+</script> -->
+
+var timeout;
+
+      // Listen for resize events
+      window.addEventListener(
+        "scroll",
+        function(event) {
+          if (scrollchecker.offsetTop < window.scrollY + 56) {
+            masthead.classList.add("opaque");
+          } else {
+            masthead.classList.remove("opaque");
+          }
+
+          // If there's a timer, cancel it
+          if (timeout) {
+            window.cancelAnimationFrame(timeout);
+          }
+
+          // Setup the new requestAnimationFrame()
+          timeout = window.requestAnimationFrame(function() {
+            // Run our scroll functions
+            console.log("debounced");
+          });
+        },
+        false
+      );
